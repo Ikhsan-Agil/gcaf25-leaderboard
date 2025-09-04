65f30cbe299260286631990f5eb50fe0da74c8d7
Ikhsan-Agil <ikhsankusuma97@gmail.com>
2025-09-04 17:00:59 +0700

    revise the date format on Last Taken

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     data = fetch.file_data
-    date = datetime.datetime.now().strftime("%d-%m-%Y at %H:%M:%S")
+    date = datetime.datetime.now().strftime("%d-%m-%Y")
     return render_template("index.html", data = data, date = date)
 
 if __name__ == "__main__":
